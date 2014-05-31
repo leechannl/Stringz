b4730ed0bb269302704d73746f5689b480f0c36b
leechannl <leechannl@gmail.com>
2014-05-31 14:13:22 +0800

    Writing data to file.

--- Stringz/main.m
+++ Stringz/main.m
@@ -42,6 +42,27 @@ int main(int argc, const char * argv[])
             NSLog(@"resolv.conf looks like this: %@", str_in);
         }
 
+        NSURL *url = [NSURL URLWithString:@"http://www.google.com/images/logos/ps_logo2.png"];
+        NSURLRequest *request = [NSURLRequest requestWithURL:url];
+        NSData *data = [NSURLConnection sendSynchronousRequest:request
+                                             returningResponse:NULL
+                                                         error:&error];
+        if (!data) {
+            NSLog(@"fetch failed: %@", [error localizedDescription]);
+            return 1;
+        }
+
+        NSLog(@"the file is %lu bytes", [data length]);
+
+        BOOL written = [data writeToFile:@"/tmp/google.png" options:0 error:&error];
+
+        if (!written) {
+            NSLog(@"write failed: %@", [error localizedDescription]);
+            return 1;
+        }
+
+        NSLog(@"success!");
+
     }
     return 0;
 }
