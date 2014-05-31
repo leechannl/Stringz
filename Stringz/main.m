92230791d03c8f4cc545dfaf3663ffb080983b3d
leechannl <leechannl@gmail.com>
2014-05-31 13:49:32 +0800

    Simple writing string to file.

--- Stringz/main.m
+++ Stringz/main.m
@@ -12,10 +12,17 @@ int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
-        
-        // insert code here...
-        NSLog(@"Hello, World!");
-        
+
+        NSMutableString *str = [[NSMutableString alloc] init];
+        for (int i = 0; i < 10; i++) {
+            [str appendString:@"Aaron is cool!\n"];
+        }
+        [str writeToFile:@"/tmp/cool.txt"
+              atomically:YES
+                encoding:NSUTF8StringEncoding
+                   error:NULL];
+        NSLog(@"done writing /tmp/cool.txt");
+
     }
     return 0;
 }
